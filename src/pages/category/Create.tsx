d9e79c065fd8d12504d08ed4875b085d0f7b6658
dwivedi-ankita <ankita.dwivedi@neosoftmail.com>
2022-12-09 10:17:16 +0530

    Create Category UI Modified

--- src/pages/category/Create.tsx
+++ src/pages/category/Create.tsx
@@ -28,73 +28,78 @@ export default function Create() {
   };
   return (
     <div>
-      <Card>
-        <Card.Header>CreateCategory</Card.Header>
-        <Card.Body>
-          <h2>Request</h2>
-          <div>
-            <div className="align-items-center">
-              <form onSubmit={submit} data-testid="form-input">
-                <label>Name :</label>
-                <input
-                  type="text"
-                  id="name"
-                  placeholder="Enter Category Name"
-                  name="name"
-                  value={apisForm.name}
-                  onChange={validateForm}
-                  required
-                />
-                <button
-                  className="btn btn-sm btn-success btn-md d-flex float-right mb-3 mr-3"
-                  type="submit"
-                >
-                  Execute
-                </button>
-              </form>
-            </div>
-            <br></br>
-            <hr></hr>
+      <div>
+        <Card>
+          <Card.Header>
+            <h2>Create Category</h2>
+          </Card.Header>
+          <Card.Body>
             <div>
-              <Card>
-                <Card.Header>
-                  <h2>Request & Response data</h2>
-                </Card.Header>
-                <Card.Body>
-                  <div>
-                    <div>
-                      <h4>Request Url</h4>
-                      <i>
-                        {process.env.REACT_APP_API_Backend}
-                        {!state.loading && state.url}
-                      </i>
-                    </div>
-                    <hr />
-                    <div>
-                      <h4>Request Payload</h4>
-                      {JSON.stringify(apisForm, undefined, 4)}
-                    </div>
-                    <hr />
-                    <div>
-                      <h4>Response data</h4>
-                      <div>
-                        {!state.loading && (
-                          <textarea
-                            rows={11}
-                            cols={60}
-                            defaultValue={JSON.stringify(state, undefined, 4)}
-                            readOnly
-                          ></textarea>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                </Card.Body>
-              </Card>
+              <div className="align-items-center">
+                <form onSubmit={submit} data-testid="form-input">
+                  <label>
+                    <b>Name: </b>
+                  </label>
+                  <input
+                    type="text"
+                    className="ml-2"
+                    id="name"
+                    placeholder="Enter Category Name"
+                    name="name"
+                    value={apisForm.name}
+                    onChange={validateForm}
+                    required
+                  />
+                  <button
+                    className="btn btn-sm btn-success btn-md d-flex float-right mb-3 mr-3"
+                    type="submit"
+                  >
+                    Create
+                  </button>
+                </form>
+              </div>
             </div>
-          </div>
-        </Card.Body>
-      </Card>
+          </Card.Body>
+        </Card>
+      </div>
+      <br></br>
+      <div>
+        <Card>
+          <Card.Header>
+            <h2>Request & Response data</h2>
+          </Card.Header>
+          <Card.Body>
+            <div>
+              <div>
+                <h4>Request Url</h4>
+                <i>
+                  {process.env.REACT_APP_API_Backend}
+                  {!state.loading && state.url}
+                </i>
+              </div>
+              <hr />
+              <div>
+                <h4>Request Payload</h4>
+                {JSON.stringify(apisForm, undefined, 4)}
+              </div>
+              <hr />
+              <div>
+                <h4>Response data</h4>
+                <div>
+                  {!state.loading && (
+                    <textarea
+                      rows={11}
+                      cols={60}
+                      defaultValue={JSON.stringify(state, undefined, 4)}
+                      readOnly
+                    ></textarea>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
